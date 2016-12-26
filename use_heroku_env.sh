4ec21593ef489519a5848c8b788e3e54b16b3138
Jeff <jeffreyh192@gmail.com>
2016-12-25 19:23:11 -0800

    use_heroku_env.sh script a bit more complete

--- use_heroku_env.sh
+++ use_heroku_env.sh
@@ -1,26 +1,101 @@
 #/usr/bin/env bash
 
+###############################################################################
+#
+# This script is for exporting heroku config vars to your local environment.
+#
+# Use wisely...
+#
+###############################################################################
+
+# -------------------------------------
+# Variables
+# -------------------------------------
 TMPFILE="$2-config.sh"
+APP=
+RESET_FLAG=
+EXPORT_FLAG=
+VERBOSE_FLAG=
+
+# -------------------------------------
+# Helper Functions
+# -------------------------------------
 function cleanup {
-    rm -rf $TMPFILE    
+    rm -rf $TMPFILE
 }
 
-while test $# -gt 0; do
-    case "$1" in
+function print_help() {
+    echo "This script is used for export heroku config variables to your"\
+         "local environment. \nUse wisely..."
+    echo ""
+    echo "options:"
+    echo "-h, --help       show help menu"
+    echo "--app            specify heroku app"
+    echo "--export         export heroku config vars to a file"
+    echo "--file           specify file to export to (default=$TMPFILE)"
+}
+
+# Run this in a subshell so we can capture the return code and cleanup
+function main {
+    # -------------------------------------
+    # Get Arguments
+    # -------------------------------------
+
+    while [[ $# -gt 0 ]]
+    do
+    key="$1"
+    case $key in
         -h|--help)
-            echo "$0 [options]"
-            echo " "
-            echo "options:"
-            echo "-h, --help       show brief help"
-            echo "--app            specify app to export config from"
-            echo "--reset          clear config vars exported by this app"
-            exit 0
+            print_help
+            return 0
             ;;
         --app)
-            touch $TMPFILE
-            heroku config --app $2 > $TMPFILE
-            exit 0
+            APP=$2
+            shift
             ;;
+        --reset)
+            RESET_FLAG="true"
+            ;;
+        --export)
+            EXPORT_FLAG="true"
+            ;;
+        --file)
+            TMPFILE=$2
+            shift
+            ;;
+        *)
+            # unknown option
+            echo "Error: Unknown option " $1 $1
+            echo ""
+            print_help
+            return 1
+        ;;
     esac
-done
+    shift # past argument or value
+    done
 
+    # If no app is specified just exit
+    if [ -z "$APP" ]; then
+        echo "ERROR: app was not specified"
+        echo ""
+        print_help
+        return 1
+    fi
+
+    # -------------------------------------
+    # Export Config Vars
+    # -------------------------------------
+    if [ -n "$EXPORT_FLAG" ]; then
+        echo "Exporting heroku app $APP to config vars to: "$TMPFILE
+        cleanup
+        touch $TMPFILE
+        heroku config -s --app $APP | sed -e "/^[A-Z_]*=/s/^/export /" > $TMPFILE
+    else
+        echo "Exporting heroku app $APP to config vars to: local environment"
+        cleanup
+        touch $TMPFILE
+        heroku config -s --app $APP | sed -e "/^[A-Z_]*=/s/^/export /" > $TMPFILE
+        source $TMPFILE
+        cleanup
+    fi
+} || cleanup
