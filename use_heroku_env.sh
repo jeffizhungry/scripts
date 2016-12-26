ebc4ce4f4326cd935bcb95bb7efd4f4bdfa24cf3
Jeff <jeffreyh192@gmail.com>
2016-12-25 20:07:10 -0800

    cleaned up use_heroku_env.sh script

--- use_heroku_env.sh
+++ use_heroku_env.sh
@@ -15,6 +15,7 @@ TMPFILE="$2-config.sh"
 APP=
 RESET_FLAG=
 EXPORT_FLAG=
+SHOW_FLAG=
 VERBOSE_FLAG=
 
 # -------------------------------------
@@ -31,11 +32,20 @@ function print_help() {
     echo "options:"
     echo "-h, --help       show help menu"
     echo "--app            specify heroku app"
-    echo "--export         export heroku config vars to a file"
+    echo ""
+    echo "--show           show horoku app config vars in local env"
+    echo "--reset          clear heroku app config vars in local env"
+    echo "--export         export heroku app config vars to a file"
     echo "--file           specify file to export to (default=$TMPFILE)"
+    echo ""
+    echo "examples:"
+    echo "  source $0 --app <myapp>          # export config vars to local env"
+    echo "  source $0 --app <myapp> --reset  # clear config vars from local env"
+    echo "  $0 --app <myapp> --show          # show config vars in local env"
+    echo "  $0 --app <myapp> --export        # export config vars to file"
+    echo ""
 }
 
-# Run this in a subshell so we can capture the return code and cleanup
 function main {
     # -------------------------------------
     # Get Arguments
@@ -53,6 +63,9 @@ function main {
             APP=$2
             shift
             ;;
+        --show)
+            SHOW_FLAG="true"
+            ;;
         --reset)
             RESET_FLAG="true"
             ;;
@@ -65,7 +78,7 @@ function main {
             ;;
         *)
             # unknown option
-            echo "Error: Unknown option " $1 $1
+            echo "Error: Unknown option " $1 $2
             echo ""
             print_help
             return 1
@@ -85,17 +98,41 @@ function main {
     # -------------------------------------
     # Export Config Vars
     # -------------------------------------
-    if [ -n "$EXPORT_FLAG" ]; then
-        echo "Exporting heroku app $APP to config vars to: "$TMPFILE
+    if [ -n "$SHOW_FLAG" ]; then
+        echo "-- Show heroku app $APP config vars found in local environment"
+        echo ""
+        cleanup
+        touch $TMPFILE
+        heroku config -s --app $APP | python -c "import sys; import re; [sys.stdout.write('echo ' + re.match('^[A-Z_]*', line).group() + '=$' + re.match('^[A-Z_]*', line).group()+'\n') for line in sys.stdin if re.match('^[A-Z_]*=', line)]" > $TMPFILE
+        source $TMPFILE
+        cleanup
+        return 0
+    elif [ -n "$RESET_FLAG" ]; then
+        echo "-- Reseting heroku app $APP config vars to empty strings"
+        echo ""
+        cleanup
+        touch $TMPFILE
+        heroku config -s --app $APP | sed -e "/^[A-Z_]*=/s/^/export /" | python -c "import sys; import re; [sys.stdout.write(re.match('^export [A-Z_]*=', line).group()+'\n') for line in sys.stdin if re.match('^export', line)]" > $TMPFILE
+        source $TMPFILE
+        cleanup
+        return 0
+    elif [ -n "$EXPORT_FLAG" ]; then
+        echo "-- Exporting heroku app $APP config vars to: "$TMPFILE
+        echo ""
         cleanup
         touch $TMPFILE
         heroku config -s --app $APP | sed -e "/^[A-Z_]*=/s/^/export /" > $TMPFILE
+        return 0
     else
-        echo "Exporting heroku app $APP to config vars to: local environment"
+        echo "-- Exporting heroku app $APP config vars to: local environment"
+        echo ""
         cleanup
         touch $TMPFILE
         heroku config -s --app $APP | sed -e "/^[A-Z_]*=/s/^/export /" > $TMPFILE
         source $TMPFILE
         cleanup
+        return 0
     fi
-} || cleanup
+} 
+
+main "$@" || cleanup
